c86b2e56bc244ddd6145d30d9a41b634face37a7
benetherington <ben.j.etherington@gmail.com>
2021-09-25 09:18:19 -0400

    popup: implement feedback to user, styling

--- popup/actions.js
+++ popup/actions.js
@@ -1,30 +1,39 @@
 const VINREGEX = RegExp("^[A-HJ-NPR-Z0-9]{3}[A-HJ-NPR-Z0-9]{5}[0-9X][A-HJ-NPR-Z0-9][A-HJ-NPR-Z0-9][A-HJ-NPR-Z0-9]{6}$", "i");
 
+/*----------------*\
+  USER INTERACTION  
+\*----------------*/
+// MOUSE CLICKS
 document.addEventListener("click", (event) =>{
+    // check which element was clicked
     if (event.target.classList.contains('action-search')) {
-        vinInput = document.getElementById("input-vin").value;
-        console.log("search clicked! VIN is: "+vinInput);
-        if (VINREGEX.test(vinInput)) {
-            console.log("sending message")
+        // SEARCH BUTTON
+        vinInput = document.getElementById("input-vin");
+        if (VINREGEX.test(vinInput.value)) {
+            // that VIN looks good, let's run a search
             browser.runtime.sendMessage(
                 { type: "popup-action",
                 values: [{
                     action: "search",
-                    vin: vinInput }]}
+                    vin: vinInput.value }]}
             );
         } else {
+            // that VIN looks bad, and somehow you were still able to click the button.
             errorMessage = document.querySelector("#error-content")
             vinField = document.getElementById("input-vin")
+            // unhide error message
             errorMessage.classList.remove("hidden");
-            vinField.classList.remove("error-attention");
+            // wiggle the text input
+            vinField.classList.remove("error-attention"); // in case it's already moving
             vinField.classList.add("error-attention");
+            // hide the error message after 4 seconds
             setTimeout( ()=>{
                 errorMessage.classList.add("hidden");
                 vinField.classList.remove("error-attention");
             }, 4*1000)
         };
     } else if (event.target.classList.contains('action-download')) {
-        console.log("download clicked!");
+        // DOWNLOAD BUTTON
         browser.runtime.sendMessage(
             { type: "popup-action",
             values: [{
@@ -33,33 +42,90 @@ document.addEventListener("click", (event) =>{
     };
 });
 
+// VIN TEXT ENTRY
+window.onload = () => {
+    // validate VIN and enable/disable search button
+    let inputVin = document.getElementById('input-vin')
+    let inputSearch = document.getElementById('input-search')
+    inputVin.addEventListener('input', (event)=>{
+        if (VINREGEX.test(inputVin.value)) {
+            inputSearch.classList.remove('disabled')
+        } else {
+            inputSearch.classList.add('disabled')
+        }
+    });
+}
+// VIN AUTO-FILL
+window.onfocus = async () => {
+    // auto-fill VIN if clipboard matches
+    let clipboardContents = await navigator.clipboard.readText() // TODO: to request access, we need to load actions.html in a new tab
+    if (VINREGEX.test(clipboardContents)) {
+        document.getElementById('input-vin').value = clipboardContents
+        document.getElementById('input-search').classList.remove('disabled')
+    }
+}
+/*--------*\
+  FEEDBACK  
+\*--------*/
+browser.runtime.onMessage.addListener((message)=>{
+    if (message.type === "feedback") {
+        for (value of message.values) {
+            feedbackHandler(value)
+        }
+        return Promise.resolve('done');
+    };
+    return false;
+});
+function feedbackHandler(feedback) {
+    switch (feedback.action) {
+        case "download-started": // feedback.tabs
+            downloadButton.update("downloading")
+        // break; case "download-tab": // feedback.images
+        // break; case "tab-increment":
+        // break; case "download-nearly-finished":
+        break; case "download-finished":
+            downloadButton.update("enabled")
+        break; case "download-abort": // feedback.display
+            downloadButton.update("disabled")
+    }
+}
 
-// /**
-// * Just log the error to the console.
-// */
-// function reportError(error) {
-//     console.error(`Error in actions popup: ${error}`);
-// }
-
-
-// /**
-// * There was an error executing the script.
-// * Display the popup's error message, and hide the normal UI.
-// */
-// function reportExecuteScriptError(error) {
-//     document.querySelector("#popup-content").classList.add("hidden");
-//     document.querySelector("#error-content").classList.remove("hidden");
-//     console.error(`Failed to execute beastify content script: ${error.message}`);
-// }
-
-// /**
-// * When the popup loads, inject a content script into the active tab,
-// * and add a click handler.
-// * If we couldn't inject the script, handle the error.
-// */
-// // browser.tabs.executeScript({file: "/content_scripts/beastify.js"})
-// // .then(listenForClicks)
-// // .catch(reportExecuteScriptError);
+downloadButton = new class {
+    constructor() {
+        this.status = 'disabled'
+        this.lookForSalvageTabs()
+    }
+    async lookForSalvageTabs() {
+        console.log("looking for salvage tabs")
+        let salvageTabs = await browser.tabs.query(
+            {active: true,
+                url: ["*://*.iaai.com/vehicledetails/*",
+                      "*://*.iaai.com/Vehicledetails?*",
+                      "*://*.copart.com/lot/*"]}
+        )
+        if (salvageTabs.length) {
+            browser.runtime.sendMessage("found a tab!")
+            this.update("enabled")
+        } else {
+            this.update("disabled")
+        }
+    }
+    update(status=null) {
+        let button = document.getElementById("input-download")
+        this.status = status || this.status
+        switch (this.status) {
+            case "disabled":
+                button.classList.add("disabled");
+                button.classList.remove("feedback-download");
+            break; case "enabled":
+                button.classList.remove("disabled");
+                button.classList.remove("feedback-download");
+            break; case "downloading":
+                button.classList.remove("disabled");
+                button.classList.add("feedback-download");
+        }
+    }
+}
 
 console.log("popup action loaded!")
 
