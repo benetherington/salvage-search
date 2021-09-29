ffbac07bc267c7f2c1b1e8217f402bb20b93d432
benetherington <ben.j.etherington@gmail.com>
2021-09-29 13:09:47 -0400

    add preferences to search

--- src/search.js
+++ src/search.js
@@ -1,25 +1,27 @@
 const VINREGEX = RegExp("^[A-HJ-NPR-Z0-9]{3}[A-HJ-NPR-Z0-9]{5}[0-9X][A-HJ-NPR-Z0-9][A-HJ-NPR-Z0-9][A-HJ-NPR-Z0-9]{6}$", "i");
+const DEFAULT_SETTINGS = {
+    searchCopart: true,
+    searchIaai: true,
+    searchRow52: true
+}
 
 async function openSalvagePages(vinInput) {
-    let storage
-    await browser.storage.local.get("settings")
-        .then(s=>storage=s.settings);
-    // TODO: add settings defaults
+    let storage = await browser.storage.local.get("settings")
+    let settings = storage.settings || DEFAULT_SETTINGS
 
     // test the clipboard text out of an abundance of caution since we need to do code injection for Copart.
     vinInput = encodeURIComponent(vinInput).replace(/^\s+|\s+$/g, '');
     if (!VINREGEX.test(vinInput)) {
-        // browser.tabs.create({'url':'malformed.html'});
         console.log("Heyyyy, how did this bad input get through?")
         return;
     };
-    if ((typeof settings === 'undefined') || settings.copart) {
+    if (settings.searchCopart) {
         openCopart(vinInput);
     };
-    if ((typeof settings === 'undefined') || settings.iaai) {
+    if (settings.searchIaai) {
         openIaai(vinInput);
     };
-    if ((typeof settings === 'undefined') || settings.row52) {
+    if (settings.searchRow52) {
         openRow52(vinInput);
     };
 
