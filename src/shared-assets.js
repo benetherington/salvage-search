cebfcb85273bb4ea4089c536f1353e9cf789c665
benetherington <ben.j.etherington@gmail.com>
2021-11-13 13:11:14 -0500

    Add vehicleABC

--- src/shared-assets.js
+++ src/shared-assets.js
@@ -7,6 +7,70 @@ const DEFAULT_SETTINGS = {
     searchPoctra: true,
     searchBidfax: true
 }
+const VehicleABC = class {
+    TO_SERIALIZE = ["tabId",
+                    "vin",
+                    "salvage",
+                    "lotNumber",
+                    "listingUrl",
+                    "imageInfo",
+                    "imageUrls"]
+    constructor(values=null) {
+        this.TO_SERIALIZE.forEach(k=> this[k]=null)
+        if (values) {Object.assign(this, values)}
+    }
+    
+    //INPUT
+    onMessage(message) {
+        message.values.vin       = this.validateVin(message.values.vin)
+        message.values.lotNumber = this.validateLot(message.values.lotNumber)
+        Object.assign(this, message.values)
+    }
+    validateVin(vin) {
+        if (!vin) {return}
+        let safe = encodeURIComponent(vin.replace(/^\s+|\s+$/g, ""));
+        if (VINREGEX.test(safe)) {return safe}
+    }
+    validateLot(lot) {
+        if (!lot) {return}
+        let safe = encodeURIComponent(lot.replace(/\D/g, ""));
+        if (LOTREGEX.test(safe)) {return safe}
+    }
+    
+    // OUTPUT
+    serialize(overrides) {
+        let serialized = Object.fromEntries(this.TO_SERIALIZE.map(k=>[k, this[k]]))
+        if (Unserializable.isPrototypeOf(this.salvage)) {
+            serialized.salvage = this.salvage.NAME;
+        }
+        Object.assign(serialized, overrides)
+        Object.setPrototypeOf(serialized, VehicleABC)
+        return serialized;
+    }
+    async getTab() {
+        if (this.tabId) {return await browser.tabs.get(this.tabId)}
+    }
+    imageUrlsCount() {
+        if (!this.imageUrls) {return 0}
+        return this.imageUrls.length
+    }
+}
+
+class BackgroundVehicle extends VehicleABC {
+    reply(options={}) {
+        this.port.postMessage({values: this.serialize(), ...options})
+    }
+    setPort(port) {
+        this.port = port;
+        port.onMessage.addListener(this.onMessage.bind(this))
+    }
+    
+}
+
+let Unserializable = {}
+let Salvage = {__proto__:Unserializable};
+let Archive = {__proto__:Unserializable};
+
 const defaultedSettings = async () => {
     let storage = await browser.storage.local.get("settings");
     let settings = storage.settings || new Object;
