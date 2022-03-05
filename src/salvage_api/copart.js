1d293c46d41e1799c36504cde3a097749579b73c
benetherington <ben.j.etherington@gmail.com>
2022-03-05 09:21:35 -0500

    move workstation

--- src/salvage_api/copart.js
+++ src/salvage_api/copart.js
@@ -85,60 +85,159 @@ const COPART_D = {
         const lotNumber = (await lotExecuting)[0];
         return lotNumber;
     },
-    lotNumberValid: async (lotNumberOrVehicle)=>{
-        let imageInfo = await COPART_D.imageInfoFromLotNumber(lotNumberOrVehicle);
-        return Object.entries(imageInfo.imageInfo.data.imagesList).length;
+    fetchImageUrls: async (lotNumber, notify=sendNotification) => {
+        return new Promise(async (resolve, reject)=>{
+            try {
+                // Fetch image info
+                const imageInfo = await COPART_D.imageInfoFromLotNumber(lotNumber);
+                console.log(imageInfo)
+                
+                // Fetch image data
+                const imageUrls = await COPART_D.imageUrlsFromInfo(imageInfo);
+                console.log(imageUrls)
+                
+                // Done!
+                resolve(imageUrls)
+            } catch (error) {
+                console.log(`COPART rejecting: ${error}`)
+                notify(`COPART: ${error}`, {displayAs:"error"})
+                reject()
+            }
+        })
     },
-    imageInfoFromLotNumber: async (lotNumberOrVehicle)=>{
-        let lotNumber;
-        if (lotNumberOrVehicle instanceof DownloadableVehicle) {
-            lotNumber = await lotNumberOrVehicle.getLotNumber();
-        } else { lotNumber = lotNumberOrVehicle; }
-        let imagesUrl = `https://www.copart.com/public/data/lotdetails/solr/lotImages/${lotNumber}/USA`;
-        let headers = { "User-Agent": window.navigator.userAgent,
-                        "Accept": "application/json, text/plain, */*" }
-        let response = await fetch(imagesUrl, headers)
-        let imageInfo;
-        if (response.headers.get("content-type").startsWith("application/json")) {
-            imageInfo = await response.json();
-        } else {
+    imageInfoFromLotNumber: async (lotNumber)=>{
+        // Configure image download
+        const imagesUrl = `https://www.copart.com/public/data/lotdetails/solr/lotImages/${lotNumber}/USA`;
+        const headers = {
+            "User-Agent": window.navigator.userAgent,
+            "Accept": "application/json, text/plain, */*"
+        };
+        
+        // Fetch image info
+        let response = await fetch(imagesUrl, {headers});
+
+        // Check status
+        if (!response.ok) throw `something went wrong on their end: ${response.status} error.`;
+        
+        // Check response content
+        if (!response.headers.get("content-type").startsWith("application/json")) {
             console.log("Copart wants a CAPTCHA check")
             browser.tabs.create({url:"https://www.copart.com"})
             throw "please complete the CAPTCHA and try again."
-        }
-        return {imageInfo};
-    },
-    imageUrlsFromInfo: async function (imageInfoOrVehicle) {
-        let imageInfo, vehicle;
-        if (imageInfoOrVehicle instanceof DownloadableVehicle) {
-            vehicle = imageInfoOrVehicle
-            imageInfo = await vehicle.getImageInfo();
-        } else {
-            imageInfo = imageInfoOrVehicle;
-        }
-        if (!imageInfo) {throw "no lot number provided.";}
+        };
         
-        // PROCESS
-        let imageUrls = [];
-        try {
-            if (   !imageInfo.hasOwnProperty("returnCode")
-                ||  imageInfo.returnCode!=1
-                || !imageInfo.hasOwnProperty("data")
-                || !imageInfo.data.hasOwnProperty("imagesList") )
-            {throw "encountered a server error."}
+        // Get response content
+        return await response.json();
+    },
+    imageUrlsFromInfo: async function (imageInfo) {
+        // Validate imageInfo
+        const nope = ()=>{throw "encountered a server error."}
+        if (!imageInfo.hasOwnProperty("returnCode")) nope();
+        if ( imageInfo.returnCode!=1               ) nope();
+        if (!imageInfo.data                        ) nope();
+        if (!imageInfo.data.imagesList             ) nope();
+        if (!imageInfo.data.imagesList.FULL_IMAGE  ) nope();
+        
+        // Pick highest resolution images
+        const imageUrls = COPART_D.pickBestImages(imageInfo);
+        return imageUrls;
+    },
+    pickBestImages: (imageInfo)=>{
+        // Grab image resolutions
+        const highs = imageInfo.data.imagesList.HIGH_RESOLUTION_IMAGE;
+        const fulls = imageInfo.data.imagesList.FULL_IMAGE;
+        
+        // Pick the highest resolution for each
+        const bestUrls = [];
+        for (let full of fulls) {
+            // Use full_res_image if a high_res is not available
+            if (!full.highRes) {
+                bestUrls.push(full.url)
+                continue;
+            }
             
-            imageUrls = imageInfo.data.imagesList.FULL_IMAGE.map(full=>{
-                if (full.highRes) { // this means "a high res is present"
-                    let high = imageInfo.data.imagesList
-                                .HIGH_RESOLUTION_IMAGE
-                                .find(high=>high.sequenceNumber===full.sequenceNumber)
-                    return high.url;
-                } else {
-                    return full.url;
-                }
-            })
-                
-        } catch (error) {throw `Copart: lot #${lotNumber} ${error}`}
-        return {imageUrls}
+            // Get corresponding high_res_image
+            const num = full.sequenceNumber;
+            const high = highs.find(i=>i.sequenceNumber===num)
+            
+            // Check high_res_image
+            if (!high) {bestUrls.push(full.url); continue}
+            
+            // Use high_res_image
+            bestUrls.push(high.url)
+        }
+        
+        return bestUrls;
     }
-}
+};
+
+// ImageInfo looks like:
+// {
+//     returnCode: 1,
+//     returnCodeDesc: "Success",
+//     data: {
+//         lotDetails: null,
+//         imagesList: {
+//             HIGH_RESOLUTION_IMAGE: [
+//                 {
+//                     url: "https://cs.copart.com/v1/AUTH_svc.pdoc00001/HPX93/f3aba7e6-f488-4fe4-aa47-11309988ced0.JPG",
+//                     imageType: "H",
+//                     sequenceNumber: 1,
+//                     swiftFlag: false,
+//                     frameCount: 0,
+//                     status: "I",
+//                     imageTypeDescription: "HIGH_RESOLUTION_IMAGE",
+//                     highRes: false
+//                 }
+//             ],
+//             FULL_IMAGE: [
+//                 {
+//                     url: "https://cs.copart.com/v1/AUTH_svc.pdoc00001/PIX450/dd68c0bd-4e50-42d6-a067-3b2e1ae5abdd.JPG",
+//                     imageType: "F",
+//                     sequenceNumber: 1,
+//                     swiftFlag: false,
+//                     frameCount: 0,
+//                     status: "I",
+//                     imageTypeDescription: "FULL_IMAGE",
+//                     highRes: true
+//                 }
+//             ],
+//             THUMBNAIL_IMAGE: [
+//                 {
+//                     url: "https://cs.copart.com/v1/AUTH_svc.pdoc00001/PIX450/3ab2c19a-b02d-489b-822d-080219171da5.JPG",
+//                     imageType: "T",
+//                     sequenceNumber: 1,
+//                     swiftFlag: false,
+//                     frameCount: 0,
+//                     status: "I",
+//                     imageTypeDescription: "THUMBNAIL_IMAGE",
+//                     highRes: false
+//                 }
+//             ],
+//             EXTERIOR_360: [
+//                 {
+//                     url: "https://c-static.copart.com/v1/AUTH_svc.pdoc00001/LPP236/eab3b74477b74d0e89e7a6210d2841a2_frames_0.jpg",
+//                     imageType: "EXT360",
+//                     sequenceNumber: 11,
+//                     swiftFlag: false,
+//                     frameCount: 55,
+//                     status: "I",
+//                     imageTypeDescription: "EXTERIOR_360",
+//                     highRes: false
+//                 }
+//             ],
+//             INTERIOR_360: [
+//                 {
+//                     url: "https://c-static.copart.com/v1/AUTH_svc.pdoc00001/LPP236/2c776bee45ed483cb9c102b465b7f8a5_O.jpeg",
+//                     imageType: "INT360",
+//                     sequenceNumber: 12,
+//                     swiftFlag: false,
+//                     frameCount: 0,
+//                     status: "I",
+//                     imageTypeDescription: "INTERIOR_360",
+//                     highRes: false
+//                 }
+//             ]
+//         }
+//     }
+// }
