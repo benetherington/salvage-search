9b61ef338b877573af629badae2609f7fcedaefe
benetherington <ben.j.etherington@gmail.com>
2022-04-08 20:11:22 -0400

    Refactor panorama content script

--- src/panorama/composer.js
+++ src/panorama/composer.js
@@ -1,14 +1,14 @@
 /*---------------------*\
   THUMBNAIL EDIT/DELETE
 \*---------------------*/
-async function saveThumb(e) {
+async function saveThumb() {
     // get thumbnail
     let thumbnail = await document.querySelector("#stage").getThumbnail()
     // add thumbnail to tray
     document.querySelector("#thumbs").append(thumbnail)
 }
 
-function downloadImages(e) {
+function downloadImages() {
     document.querySelectorAll("#thumbs img").forEach((img, idx)=>{
         browser.downloads.download({
             url:img.src,
@@ -18,29 +18,43 @@ function downloadImages(e) {
     })
 }
 
+
+/*-----------*\
+  HELP BUTTON
+\*-----------*/
+const toggleHelpPopup = (e)=>{
+    // Toggle popup visibility
+    const nowVisible = document.getElementById("help-popup")
+                            .classList.toggle("fade-out");
+    
+    // Add or remove "click anywhere to close" listener
+    if (nowVisible) {
+        document.body.removeEventListener("click", toggleHelpPopup)
+    } else {
+        document.body.addEventListener("click", toggleHelpPopup)
+    }
+    // Assure that the popup isn't immediately closed
+    e.stopImmediatePropagation()
+};
+
+
 /*--------------------*\
   BACKGROUND MESSAGING
 \*--------------------*/
 var salvageName, lotNumber;
 async function messageHandler(message) {
-    // console.log(message);
-    // Assign variables for download folder
+    // Assign variables for naming the download folder later
     ({salvageName, lotNumber} = message)
     
-    // Get panorama data from messagehandler
-    const {panoUrls:faces} = message;
-    
-    // Display panorama
+    // load images to the stage
     const stage = document.querySelector("#stage");
-    await stage.addPano(faces);
+    await stage.addPano(message.panoUrls);
     
-    // Create thumbnail set, point them in the correct direction
+    // Create premade thumbnails
     await stage.getPano().goToDriver();    await saveThumb();
     await stage.getPano().goToRear();      await saveThumb();
     await stage.getPano().goToPassenger(); await saveThumb();
     await stage.getPano().goToIp();        await saveThumb();
-    
-    // console.log("message handled.")
 }
 browser.runtime.onMessage.addListener(messageHandler);
 
@@ -65,8 +79,11 @@ window.addEventListener("load", ()=>{
     document.querySelector("#dl-all").addEventListener("click", downloadImages)
     document.getElementById("help").addEventListener("click", toggleHelpPopup)
     
-    // STAGE
-    document.querySelector("#stage").addEventListener("click", (e)=>{
-        if (e.detail===2) {saveThumb()}
+    // THUMBNAIL CREATION
+    document.addEventListener("keydown", (e)=>{
+        if (e.key==="Enter") saveThumb();
+    })
+    document.getElementById("stage").addEventListener("click", (e)=>{
+        if (e.detail===2) saveThumb();
     })
 })
