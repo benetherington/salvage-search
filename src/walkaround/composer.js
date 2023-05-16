0984fc81b2ec8b1fe2d1e5a035801c58398f87bb
Ben Etherington <ben.j.etherington@gmail.com>
2023-05-16 12:01:06 -0400

    Add file input to walkaround

--- src/walkaround/composer.js
+++ src/walkaround/composer.js
@@ -3,15 +3,15 @@
 \*-----------------------*/
 function saveThumb() {
     // Get thumbnail
-    let thumbnail = document.querySelector("#stage").getThumbnail();
+    let thumbnail = document.querySelector('#stage').getThumbnail();
 
     // Listen for thumbnail removal and update scrollbar
     thumbnail
-        .querySelector(".delete")
-        .addEventListener("click", moveThumbsScrollbar);
+        .querySelector('.delete')
+        .addEventListener('click', moveThumbsScrollbar);
 
     // Add thumbnail to tray
-    document.querySelector("#thumbs").append(thumbnail);
+    document.querySelector('#thumbs').append(thumbnail);
     moveThumbsScrollbar();
 }
 function downloadImages() {
@@ -19,10 +19,10 @@ function downloadImages() {
     let folder;
     if (salvageName && lotNumber) folder = `${salvageName}-${lotNumber}`;
     else if (salvageName || lotNumber) folder = lotNumber || salvageName;
-    else folder = "salvage_photos";
+    else folder = 'salvage_photos';
 
     // Save images
-    document.querySelectorAll("#thumbs img").forEach((img, idx) => {
+    document.querySelectorAll('#thumbs img').forEach((img, idx) => {
         browser.downloads.download({
             url: img.src,
             filename: `${folder}/exterior360/exterior-${idx}.jpg`,
@@ -35,14 +35,14 @@ function downloadImages() {
   THUMBNAIL SCROLLBAR
 \*-------------------*/
 const moveThumbsScrollbar = () => {
-    const thumbs = document.getElementById("thumbs");
+    const thumbs = document.getElementById('thumbs');
 
     // Find out how far down we've scrolled
     const scrollMax = thumbs.scrollHeight - thumbs.clientHeight;
     const barPosition = thumbs.scrollTop / scrollMax;
 
     // Stop now if there's no need to scroll
-    if (scrollMax <= 0) return thumbs.style.setProperty("--scroll-height", "0");
+    if (scrollMax <= 0) return thumbs.style.setProperty('--scroll-height', '0');
 
     // Figure out how tall the scrollbar should be
     const barHeight = thumbs.clientHeight / thumbs.scrollHeight;
@@ -56,10 +56,10 @@ const moveThumbsScrollbar = () => {
     // Set properties, including margin and padding applied to #thumbs
     const thumbsContentTop = 15;
     thumbs.style.setProperty(
-        "--scroll-height",
-        barHeightPx - thumbsContentTop + "px",
+        '--scroll-height',
+        barHeightPx - thumbsContentTop + 'px',
     );
-    thumbs.style.setProperty("--scroll-top", barTop + thumbsContentTop + "px");
+    thumbs.style.setProperty('--scroll-top', barTop + thumbsContentTop + 'px');
 };
 
 /*-----------*\
@@ -68,18 +68,32 @@ const moveThumbsScrollbar = () => {
 const toggleHelpPopup = (e) => {
     // Toggle popup visibility
     const nowVisible = document
-        .getElementById("help-popup")
-        .classList.toggle("fade-out");
+        .getElementById('help-popup')
+        .classList.toggle('fade-out');
 
     // Add or remove "click anywhere to close" listener
     if (nowVisible) {
-        document.body.removeEventListener("click", toggleHelpPopup);
+        document.body.removeEventListener('click', toggleHelpPopup);
     } else {
-        document.body.addEventListener("click", toggleHelpPopup);
+        document.body.addEventListener('click', toggleHelpPopup);
         // Assure that the popup isn't immediately closed
         e.stopImmediatePropagation();
     }
 };
+
+/*-------------------*\
+  MANUAL FILE LOADING
+\*-------------------*/
+async function loadLocalFiles() {
+    // Get files from input element
+    const files = document.getElementById('pano-files').files;
+
+    // Create object URLs
+    const walkaroundUrls = [...files].map((file) => URL.createObjectURL(file));
+
+    // "Recieve" a message
+    messageHandler({walkaroundUrls});
+}
 
 /*--------------------*\
   BACKGROUND MESSAGING
@@ -90,14 +104,14 @@ const messageHandler = (message) => {
     ({salvageName, lotNumber} = message);
 
     // Load images to the stage
-    document.querySelector("#stage").setAngles(message.walkaroundUrls);
+    document.getElementById('stage').setAngles(message.walkaroundUrls);
 };
 browser.runtime.onMessage.addListener(messageHandler);
 
 /*---------------*\
   EVENT LISTENERS
 \*---------------*/
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
     // FAKE LOADING FROM POPUP
     // let walkaroundUrls = Array.from(Array(64).keys()).map(idx=>
     //     "/walkaround/images/"+idx+".jpg"
@@ -105,21 +119,26 @@ window.addEventListener("load", () => {
     // messageHandler({walkaroundUrls})
 
     // BUTTONS
-    document.querySelector("#save-view").addEventListener("click", saveThumb);
-    document.querySelector("#dl-all").addEventListener("click", downloadImages);
-    document.getElementById("help").addEventListener("click", toggleHelpPopup);
+    document.getElementById('save-view').addEventListener('click', saveThumb);
+    document.getElementById('dl-all').addEventListener('click', downloadImages);
+    document.getElementById('help').addEventListener('click', toggleHelpPopup);
+
+    // FILES
+    document
+        .getElementById('pano-files-load')
+        .addEventListener('click', loadLocalFiles);
 
     // THUMBNAIL CREATION
-    document.addEventListener("keydown", (e) => {
-        if (e.key === "Enter") saveThumb();
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') saveThumb();
     });
-    document.getElementById("stage").addEventListener("click", (e) => {
+    document.getElementById('stage').addEventListener('click', (e) => {
         if (e.detail === 2) saveThumb();
     });
 
     // THUMBS SCROLLING
     document
-        .getElementById("thumbs")
-        .addEventListener("scroll", moveThumbsScrollbar);
-    window.addEventListener("resize", moveThumbsScrollbar);
+        .getElementById('thumbs')
+        .addEventListener('scroll', moveThumbsScrollbar);
+    window.addEventListener('resize', moveThumbsScrollbar);
 });
